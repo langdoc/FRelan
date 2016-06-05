5d4d1036f3e723d26526b278ff65e965269c0c8c
nikopartanen <nikotapiopartanen@gmail.com>
2016-06-05 19:42:38 +0200

    updated this function so that it is more coincise

--- R/read_tier.R
+++ R/read_tier.R
@@ -17,31 +17,42 @@ read_tier <- function(eaf_file = "/Volumes/langdoc/langs/kpv/kpv_izva20140404Igu
                 file %>% xml2::xml_find_all(paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "']")) %>%
                         xml2::xml_attr("PARTICIPANT") -> participants_in_file
 
-                create_path <- function(..., above = F){
-
-                        restriction <- paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "']")
-
-                        if (above == T){
-                                xpath_end <- "/ANNOTATION/*/ANNOTATION_VALUE/../../.."
-                        } else {
-                                xpath_end <- "/ANNOTATION/*/ANNOTATION_VALUE"
-                        }
-
-                        paste0(restriction, xpath_end)
-                }
-
-                coerce_data_frame <- function(current_participant){
+                coerce_data_frame <- function(participant){
                         dplyr::data_frame(
-                                Content = file %>% xml2::xml_find_all(create_path()) %>% xml2::xml_text(),
-                                annot_id = file %>% xml2::xml_find_all(paste0(create_path(), "/..")) %>% xml2::xml_attr("ANNOTATION_ID"),
-                                ref_id = file %>% xml2::xml_find_all(paste0(create_path(), "/..")) %>% xml2::xml_attr("ANNOTATION_REF"),
-                                participant = file %>% xml2::xml_find_all(create_path(above = T)) %>% xml2::xml_attr("PARTICIPANT"),
-                                tier_id = file %>% xml2::xml_find_all(create_path(above = T)) %>% xml2::xml_attr("TIER_ID"),
-                                type = file %>% xml2::xml_find_all(create_path(above = T)) %>% xml2::xml_attr("LINGUISTIC_TYPE_REF"),
-                                time_slot_1 = file %>% xml2::xml_find_all(create_path(above = T)) %>% xml2::xml_attr("TIME_SLOT_REF1"),
-                                time_slot_1 = file %>% xml2::xml_find_all(create_path(above = T)) %>% xml2::xml_attr("TIME_SLOT_REF2"))
+                                content = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE")) %>%
+                                        xml2::xml_text(),
+                                annot_id = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/..")) %>%
+                                        xml2::xml_attr("ANNOTATION_ID"),
+                                ref_id = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/..")) %>%
+                                        xml2::xml_attr("ANNOTATION_REF"),
+                                participant = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/../../..")) %>%
+                                        xml2::xml_attr("PARTICIPANT"),
+                                tier_id = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/../../..")) %>%
+                                        xml2::xml_attr("TIER_ID"),
+                                type = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/../../..")) %>%
+                                        xml2::xml_attr("LINGUISTIC_TYPE_REF"),
+                                time_slot_1 = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/../../..")) %>%
+                                        xml2::xml_attr("TIME_SLOT_REF1"),
+                                time_slot_2 = file %>%
+                                        xml2::xml_find_all(
+                                                paste0("//TIER[@LINGUISTIC_TYPE_REF='", linguistic_type, "' and @PARTICIPANT='", participant,"']/ANNOTATION/*/ANNOTATION_VALUE/../../..")) %>%
+                                        xml2::xml_attr("TIME_SLOT_REF2"))
                 }
 
-                plyr::ldply(participants_in_file, coerce_data_frame) %>% dplyr::tbl_df
+                plyr::ldply(participants_in_file, coerce_data_frame) %>% dplyr::tbl_df()
 
         }
