a9916dc8f97d5a2531c5f80f0ccf0f23c686f8f4
nikopartanen <nikotapiopartanen@gmail.com>
2016-06-05 19:00:20 +0200

    small fix with example

--- R/is_tier_independent.R
+++ R/is_tier_independent.R
@@ -1,12 +1,12 @@
 #' is_tier_independent Function checks whether a linguistic type is independent or not
 #'
-#' This function checks the linguistic type definition and returns information about whether it is possible to align the tier or not. This is important in situations where one has to find out whether there should be attributes for time codes, and it can also be used to check that tiers are set up as they should.
+#' This function checks the linguistic type definition and returns information about whether it is possible to align the tier or not.
 #' @param eaf_file This is a path to the file.
 #' @param linguistic_type This is the linguistic type
 #' @keywords ELAN
 #' @export
 #' @examples
-#' is_tier_independent(path = "path/to/elan/file.eaf", tier = "ref@NTP-M-1986")
+#' is_tier_independent(path = "path/to/elan/file.eaf", tier = "ref")
 
 is_type_independent <- function(eaf_file, linguistic_type) {
 
