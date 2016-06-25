680e46abc0a63328171f30992469945f6c490d29
nikopartanen <nikotapiopartanen@gmail.com>
2016-06-25 10:48:15 +0300

    added a function which reads language documentation note files

--- R/harvest_notes.R
+++ R/harvest_notes.R
@@ -1,18 +1,15 @@
 #' harvest_notes Function
 #'
-#' This function launches a Shiny app. Please see the documentation in the address [github.com/langdoc/FRorpus-demo](http://github.com/langdoc/FRorpus-demo).
-#' @param corpus The corpus file
-#' @keywords ELAN
+#' This function finds and parses note annotation files written in Emacs ld-notes-mode.
+#' @param path Where to look for files
+#' @keywords language documentation
 #' @export
-
-library(readr)
-library(plyr)
-suppressPackageStartupMessages(library(dplyr))
-library(yaml)
 
 harvest_notes <- function(path = "."){
 
 notes <- list.files(path = path, pattern = "ldnotes\\....", recursive = T, full.names = T)
+
+`%>%` <- dplyr::`%>%`
 
 get_header_attr <- function(note_file){
 
@@ -25,30 +22,30 @@ get_header_attr <- function(note_file){
                                   header_line_nums[2])],
                         collapse = "\n")
         # parse it as yaml, which returns a list of property values
-        header <- yaml.load(header)
+        header <- yaml::yaml.load(header)
         dplyr::data_frame(session_number = header$session_number,
                           location = header$session_location,
                           file = note_file)
 }
 
-note_headers <- ldply(notes, get_header_attr)
+note_headers <- plyr::ldply(notes, get_header_attr)
 
 
-note_text <- llply(notes, function(x) {
-        read_tsv(x, skip = 10, col_names = F) %>% rename(time_stamp = X1, annotation = X2) %>%
-                mutate(file = x)
+note_text <- plyr::llply(notes, function(x) {
+        readr::read_tsv(x, skip = 10, col_names = F) %>% dplyr::rename(time_stamp = X1, annotation = X2) %>%
+                dplyr::mutate(file = x)
 })
 
-note_content <- ldply(note_text, function(x){
-        left_join(x, note_headers, by = "file") %>% tbl_df
+note_content <- plyr::ldply(note_text, function(x){
+        dplyr::left_join(x, note_headers, by = "file") %>% tbl_df
 })
 
-note_content %>% tbl_df %>% filter(! is.na(annotation)) %>%
-        mutate(tag = stringr::str_replace(annotation, "(^[A-Z]+) (.+)", "\\1")) -> note_content
+note_content %>% dplyr::tbl_df %>% dplyr::filter(! is.na(annotation)) %>%
+        dplyr::mutate(tag = stringr::str_replace(annotation, "(^[A-Z]+) (.+)", "\\1")) -> note_content
 
 note_content[stringi::stri_detect_regex(note_content$annotation, "([A-Z]+) .+") == FALSE,]$tag <- NA
 
-note_content %>% mutate(annotation = stringr::str_replace(annotation, "(^[A-Z]+) (.+)", "\\2")) -> note_content
+note_content %>% dplyr::mutate(annotation = stringr::str_replace(annotation, "(^[A-Z]+) (.+)", "\\2")) -> note_content
 
 note_content
 
