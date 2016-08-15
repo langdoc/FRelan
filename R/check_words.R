e41a162d7f8d652791e439dd3b8ebf739c7740c6
nikopartanen <nikotapiopartanen@gmail.com>
2016-08-15 21:14:29 +0200

    this is a very nice function that can be used to evaluate what is inconsistent in new transcriptions

---
+++ R/check_words.R
@@ -0,0 +1,16 @@
+#' check_words Function
+#'
+#' This function takes one ELAN file reads the word tokens and compares those to the previously created data frame.
+#' The idea is to find tokens which do not occur in corpus already and are possible candidates for being typos.
+#' @param eaf_file Filename
+#' @param corpus Data frame which contains corpus to compare
+#' @keywords ELAN
+#' @export
+
+check_words <- function(eaf_file = "/Volumes/langdoc/langs/kpv/kpv_izva20150406-3/kpv_izva20150406-3-a/kpv_izva20150406-3-a.eaf", corpus_rds = "/Volumes/langdoc/langs/kpv/kpv_izva_corpus.rds"){
+
+        corpus_to_compare <- readr::read_rds(corpus) %>% tbl_df
+        new_words <- FRelan::read_tier(eaf_file = eaf_file, linguistic_type = "wordT") %>% .$content
+        sort(new_words[! new_words %in% corpus_to_compare$word])
+
+}
