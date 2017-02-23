42b0c6f6992fbe1db48d058dfd08c209de96ccf6
nikopartanen <nikotapiopartanen@gmail.com>
2017-02-23 20:50:37 +0100

    small improvements, still messy

--- R/tier_has_content.R
+++ R/tier_has_content.R
@@ -7,22 +7,25 @@
 #' @keywords ELAN
 #' @export
 #' @examples
-#' tier_exists("path/to/elan/file.eaf", "orth@NTP-M-1986")
+#' tier_has_content("path/to/elan/file.eaf", "orth@NTP-M-1986")
 
 tier_has_content <- function(eaf_file, tier, value = F){
 
+        `%>%` <- dplyr::`%>%`
+
         if (tier_exists(eaf_file = eaf_file, tier = tier) == F){
-                print("Tier doesn't exist!")
+                FALSE
         } else {
                 tier_content <- xml2::read_xml(eaf_file) %>%
                         xml2::xml_find_all(
                                 xpath = paste0("//TIER[@TIER_ID='", tier, "']/ANNOTATION/*/ANNOTATION_VALUE")) %>%
                         xml2::xml_text()
-        }
+                tier_content
+                }
 
         if (value == FALSE && tier_exists(eaf_file = eaf_file, tier = tier) == T) {
                 result <- length(tier_content) > 0
-                print(result)
+                result
         }
 
         if (value == T && tier_exists(eaf_file = eaf_file, tier = tier) == T) {
