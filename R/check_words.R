07b88e112b790afd09c23a272b67052c8ef83d2c
Niko Partanen <nikotapiopartanen@gmail.com>
2018-02-14 15:23:22 +0100

    fixed this a bit, it is actually a very useful function

--- R/check_words.R
+++ R/check_words.R
@@ -6,10 +6,12 @@
 #' @param corpus Data frame which contains corpus to compare
 #' @keywords ELAN
 #' @export
+#' @examples
+#' check_words(eaf_file = "file.eaf", corpus_rds = "corpus.rds")
 
-check_words <- function(eaf_file = "/Volumes/langdoc/langs/kpv/kpv_izva20150406-3/kpv_izva20150406-3-a/kpv_izva20150406-3-a.eaf", corpus_rds = "/Volumes/langdoc/langs/kpv/kpv_izva_corpus.rds"){
+check_words <- function(eaf_file, corpus_rds){
 
-        corpus_to_compare <- readr::read_rds(corpus) %>% tbl_df
+        corpus_to_compare <- readr::read_rds(corpus_rds) %>% tbl_df
         new_words <- FRelan::read_tier(eaf_file = eaf_file, linguistic_type = "wordT") %>% .$content
         new_words <- tolower(new_words)
         sort(new_words[! new_words %in% corpus_to_compare$token])
